67b10b8385f1e58145d261aa55782392da311120
zlendo1 <zlendo1@etf.unsa.ba>
2024-12-12 18:04:39 +0100

    merged node and tree modules | added left_rotate

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+mod rbtree;
+
 fn main() {
     println!("Hello, world!");
 }
---
+++ src/rbtree.rs
@@ -0,0 +1,61 @@
+use std::ptr::NonNull;
+
+enum Color {
+    Black,
+    Red,
+}
+
+struct Node<K> {
+    parent: Link<K>,
+    left: Link<K>,
+    right: Link<K>,
+    color: Color,
+    key: K,
+}
+
+type NodePtr<K> = NonNull<Node<K>>;
+type Link<K> = Option<NodePtr<K>>;
+type LinkPtr<K> = NonNull<Link<K>>;
+
+pub struct RbTree<K> {
+    root: Link<K>,
+}
+
+impl<K> RbTree<K> {
+    pub fn new() -> Self
+    where
+        K: Ord,
+    {
+        Self { root: None }
+    }
+
+    pub fn is_empty(&self) -> bool {
+        self.root.is_none()
+    }
+
+    pub fn left_rotate(&mut self, mut node_ptr: NodePtr<K>) {
+        unsafe {
+            if let Some(mut right_ptr) = node_ptr.as_ref().right {
+                node_ptr.as_mut().right = right_ptr.as_ref().left;
+                if let Some(mut right_left_ptr) = right_ptr.as_mut().left {
+                    right_left_ptr.as_mut().parent = Some(node_ptr);
+                }
+
+                right_ptr.as_mut().parent = node_ptr.as_ref().parent;
+                match node_ptr.as_ref().parent {
+                    None => self.root = Some(right_ptr),
+                    Some(mut parent_ptr) => {
+                        if parent_ptr.as_ref().left == Some(node_ptr) {
+                            parent_ptr.as_mut().left = Some(right_ptr);
+                        } else {
+                            parent_ptr.as_mut().right = Some(right_ptr);
+                        }
+                    }
+                }
+
+                right_ptr.as_mut().left = Some(node_ptr);
+                node_ptr.as_mut().parent = Some(right_ptr);
+            }
+        }
+    }
+}
