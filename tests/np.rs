a25821af856d3aceda7fe9ce56d46907b6876638
zlendo1 <zlendo1@etf.unsa.ba>
2025-01-08 23:10:59 +0100

    pooled tests

--- tests/np.rs
+++ tests/np.rs
@@ -1,35 +1,22 @@
 use nasp_homework::np::{Statement, CNF};
 
 #[test]
-fn verify_true() {
+fn cnf_verify() {
     let mut statement = Statement::new(3);
     statement.set(vec![false, false, false]);
 
-    let cnf = CNF::new(vec![vec![1, 2, -3]]);
+    let cnf1 = CNF::new(vec![vec![1, 2, -3]]);
+    let cnf2 = CNF::new(vec![vec![1, 2, 3]]);
 
-    assert!(cnf.verify(&statement));
+    assert!(cnf1.verify(&statement));
+    assert!(!cnf2.verify(&statement));
 }
 
 #[test]
-fn verify_false() {
-    let mut statement = Statement::new(3);
-    statement.set(vec![false, false, false]);
+fn cnf_result() {
+    let cnf1 = CNF::new(vec![vec![1, 2, 3]]);
+    let cnf2 = CNF::new(vec![vec![1, 1, 1], vec![-1, -1, -1]]);
 
-    let cnf = CNF::new(vec![vec![1, 2, 3]]);
-
-    assert!(!cnf.verify(&statement));
-}
-
-#[test]
-fn result_true() {
-    let cnf = CNF::new(vec![vec![1, 2, 3]]);
-
-    assert!(cnf.result());
-}
-
-#[test]
-fn result_false() {
-    let cnf = CNF::new(vec![vec![1, 1, 1], vec![-1, -1, -1]]);
-
-    assert!(!cnf.result());
+    assert!(cnf1.result());
+    assert!(!cnf2.result());
 }
