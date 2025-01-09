23831add932a0952731ec660c05ab1c4af691f83
zlendo1 <zlendo1@etf.unsa.ba>
2025-01-09 13:41:16 +0100

    added part of main

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,26 @@
-use std::io;
+use std::io::{self, Write};
 
-use nasp_homework::rbtree::RbTree;
+use nasp_homework::np::{Graph, CNF};
 
 fn main() {
-    let mut tree: RbTree<i32> = RbTree::new();
+    let mut cnf: Option<CNF> = None;
+    let mut graph: Option<Graph> = None;
 
     loop {
         println!("\nMenu:");
         println!("0. Exit");
-        println!("1. Insert node");
-        println!("2. Delete node");
-        println!("3. Inorder print nodes");
+        println!("1. Input formula");
+        println!("2. Input graph");
+        println!("3. Check formula satisfiability");
+        println!("4. Check if k-indset exists");
+        println!("5. Check if k-clique exists");
+        println!("6. Verify formula");
+        println!("7. Verify indset");
+        println!("8. Verify clique");
+        println!("9. Reduce 3-SAT to indset");
+        println!("10. Reduce 3-SAT to clique");
         print!("\nEnter your choice: ");
 
-        use std::io::Write;
         io::stdout().flush().unwrap();
 
         let mut choice = String::new();
@@ -28,65 +35,72 @@ fn main() {
                 break;
             }
             "1" => {
-                print!("Enter the new node: ");
-                io::stdout().flush().unwrap();
-
-                let mut element = String::new();
-                io::stdin()
-                    .read_line(&mut element)
-                    .expect("Failed to read input");
-                let element: i32 = match element.trim().parse() {
-                    Ok(num) => num,
-                    Err(_) => {
-                        println!("Invalid input. Please enter a valid integer.");
-                        continue;
-                    }
-                };
-
-                let inserted = tree.insert(element);
-
-                if inserted {
-                    println!("Node inserted successfully!");
-                } else {
-                    println!("Node already exits in tree.")
-                }
+                cnf = Some(input_cnf());
             }
             "2" => {
-                print!("Enter existing node: ");
-                io::stdout().flush().unwrap();
-
-                let mut element = String::new();
-                io::stdin()
-                    .read_line(&mut element)
-                    .expect("Failed to read input");
-                let element: i32 = match element.trim().parse() {
-                    Ok(num) => num,
-                    Err(_) => {
-                        println!("Invalid input. Please enter a valid integer.");
-                        continue;
-                    }
-                };
-
-                let key = tree.delete(&element);
-
-                if key.is_some() {
-                    println!("Node deleted successfully!");
-                } else {
-                    println!("Node does not exist.");
-                }
+                graph = Some(input_graph());
             }
             "3" => {
-                println!("\nNodes:");
-                if tree.is_empty() {
-                    println!("No nodes to display.");
+                if let Some(f) = &cnf {
+                    let satisfiable = f.result();
+
+                    println!(
+                        "Formula is {}satisfiable.",
+                        if satisfiable { "" } else { "not " }
+                    );
                 } else {
-                    let elements = tree.inorder_traverse();
-                    for element in elements.iter() {
-                        print!("{} ", *element);
-                    }
-                    println!("")
+                    println!("No formula found.");
                 }
             }
+            "4" => {
+                if let Some(g) = &graph {
+                    print!("Input k: ");
+
+                    io::stdout().flush().unwrap();
+
+                    let k = read_usize();
+                    let result = g.result_k_indset(k);
+
+                    println!(
+                        "There {} a {}-indset in the graph.",
+                        if result { "exists" } else { "does not exist" },
+                        k
+                    )
+                } else {
+                    println!("No graph found.")
+                }
+            }
+            "5" => {
+                if let Some(g) = &graph {
+                    print!("Input k: ");
+
+                    io::stdout().flush().unwrap();
+
+                    let k = read_usize();
+                    let result = g.result_k_clique(k);
+
+                    println!(
+                        "There {} a {}-clique in the graph.",
+                        if result { "exists" } else { "does not exist" },
+                        k
+                    )
+                } else {
+                    println!("No graph found.")
+                }
+            }
+            "6" => {
+                if let Some(f) = &cnf {
+                    let assignment = input_assignment();
+
+                    let valid = f.verify(assignment);
+
+                    println!("The assignment is {}valid.", if valid { "" } else { "in" })
+                }
+            }
+            "7" => {}
+            "8" => {}
+            "9" => {}
+            "10" => {}
             _ => {
                 println!("Invalid choice. Please try again.");
             }
